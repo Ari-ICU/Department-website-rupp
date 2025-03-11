d311fc1f62a530523af38159e8391b8a08ee402c
Ari-ICU <thoeurn.ratha.kh@gmail.com>
2025-03-11 15:58:09 +0700

    update

--- src/components/home/StatisticCard.jsx
+++ src/components/home/StatisticCard.jsx
@@ -4,7 +4,7 @@ const StatisticCard = ({ value, rank }) => {
   return (
     <div className="flex flex-col items-center justify-center text-center">
       <div className="text-3xl font-bold text-white">{value}</div>
-      <div className="text-2xl font-normal text-gray-300 mt-2">{rank}</div>
+      <div className="text-2xl font-normal text-gray-50 mt-2">{rank}</div>
     </div>
   );
 };
