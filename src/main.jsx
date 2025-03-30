8e6aa376c59d9e3d97e8eb1efe68dfe576355c2a
Ari-ICU <thoeurn.ratha.kh@gmail.com>
2025-03-30 17:13:39 +0700

    add new page

--- src/main.jsx
+++ src/main.jsx
@@ -4,14 +4,10 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import "./i18n";
-import { HelmetProvider } from 'react-helmet-async';
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <HelmetProvider>
-    <App />
-    </HelmetProvider>
-   
+    <App />   
   </StrictMode>,
 )
